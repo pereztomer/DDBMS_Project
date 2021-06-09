77c4433b8330a976fe8c2dd49f58e72279d57797
Tomer <tomer.perez@campus.technion.ac.il>
2021-06-09 17:54:31 +0300

    another testing

--- manege_transactions.py
+++ manege_transactions.py
@@ -50,6 +50,7 @@ def manege_transactions(T):
     spark, sc = init_spark("manege_transactions")
 
     for file_path in list_of_files:
+        transacionID = file_path.replace('C:/Users/Tomer/PycharmProjects/DDBMS_Project/orders/', '').replace('_11.csv','')
         if not file_path.endswith('_' + str(X) + '.csv'):
             print(f"File {file_path} not in correct format - was rejected")
         else:
@@ -72,7 +73,10 @@ def manege_transactions(T):
                 #########################################
                 # We need a reading lock here!!!!
                 ##########################################
-                cursor_row.execute('select productID,inventory from productsInventory where productID in' + wantedProductID)
+                reading_site_query = 'select productID,inventory from productsInventory where productID in' + wantedProductID
+                cursor_row.execute('INSERT INTO Log (timestamp, relation, transactionID, productID, action, record) \
+                 VALUES (current_date ,ProductsOrdered, transactionID, productID, read,reading_site_query)')
+                cursor_row.execute(reading_site_query)
                 # האם יש מקרה קצה בו עבור אותה קטגוריה ומוצר יש כמה דרישות בcsv?
                 for user_row in cursor_row:
                     product_amount = query.filter(query.productID == int(user_row[1])).select('amount')
@@ -90,9 +94,12 @@ def manege_transactions(T):
                 # requesting writing lock for the whole website
                 #############################################
                 # update log of accessing to productordered
+
+                cursor_row.execute('INSERT INTO Log (timestamp, relation, transactionID, productID, action, record) '
+                                   'VALUES (current_date ,ProductsOrdered, transactionID, productID, 'read',reading_site_query   )')
                 # update procutsordered of user with(our_tID,pID,ourAmount)
                 # update log of down inventory
-                # update productsinventory with inventory = invenotry - ourAmount
+                # update productsinventory with inventory = inventory - ourAmount
 
 
         #######################################################
