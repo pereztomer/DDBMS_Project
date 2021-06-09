c0fda570346aa19dc4ab2681e95ae800ccbe86cd
Tomer <tomer.perez@campus.technion.ac.il>
2021-06-09 17:25:27 +0300

    another testing

--- manege_transactions.py
+++ manege_transactions.py
@@ -4,7 +4,6 @@ import findspark
 from pyspark.sql import SparkSession
 import pyodbc
 
-
 # Constants
 X = 11
 Y = 6
@@ -15,6 +14,7 @@ from pyspark.sql import functions as F
 from pyspark.sql.window import Window
 from pyspark.sql.types import IntegerType, FloatType
 
+
 def connect_to_db(username):
     server = 'technionddscourse.database.windows.net'
     database = username
@@ -23,15 +23,10 @@ def connect_to_db(username):
     conn = pyodbc.connect(
         'DRIVER={SQL Server};'
         'SERVER=' + server + ';'
-        'DATABASE=' + database + ';'
-        'UID=' + username + ';'
-        'PWD=' + password + ';')
+                             'DATABASE=' + database + ';'
+                                                      'UID=' + username + ';'
+                                                                          'PWD=' + password + ';')
     return conn
-
-
-
-
-
 
 
 def init_spark(app_name: str):
@@ -55,26 +50,51 @@ def manege_transactions(T):
     spark, sc = init_spark("manege_transactions")
 
     for file_path in list_of_files:
-        if not file_path.endswith('_' + str(X)+'.csv'):
+        if not file_path.endswith('_' + str(X) + '.csv'):
             print(f"File {file_path} not in correct format - was rejected")
         else:
             query = spark.read.format("csv").option("header", "true").load(file_path)
             categories = list(query.select('categoryID').toPandas()['categoryID'])
-            categories = '('+','.join(str(e) for e in categories)+')'
+            categories = '(' + ','.join(str(e) for e in categories) + ')'
             conn = connect_to_db('dbteam')
             cursor = conn.cursor()
             cursor.execute('select distinct siteName, categoryID from CategoriesToSites where categoryID in' + categories)
-
+            flag = True
             for row in cursor:
-                conn_row = connect_to_db(row[0])
-                cursor_row = conn_row.cursor()
-                cursor_row.execute()
+                if not flag:
+                    break
 
-                 read lock
-                cursor.execute('select *  from productsinventory where  in invenotry AND')
-                write lock
-                cursor.execute('')
-                exit()
-#
-#
-#
+                conn_row = connect_to_db(row[0])
+                wantedProductID = query.filter(query.categoryID == str(row[1])).select('productID')
+                wantedProductID = list(wantedProductID.toPandas()['productID'])
+                wantedProductID = '(' + ','.join(str(e) for e in wantedProductID) + ')'
+                cursor_row = conn_row.cursor()
+                #########################################
+                # We need a reading lock here!!!!
+                ##########################################
+                cursor_row.execute('select productID,inventory from productsInventory where productID in' + wantedProductID)
+                # האם יש מקרה קצה בו עבור אותה קטגוריה ומוצר יש כמה דרישות בcsv?
+                for user_row in cursor_row:
+                    product_amount = query.filter(query.productID == int(user_row[1])).select('amount')
+                    product_amount_lst = list(product_amount.toPandas()['amount'])
+                    product_amount = sum(product_amount_lst)
+                    if user_row[1] < product_amount:
+                        print(f"Query {file_path} can not be completed")
+                        ###############################################
+                        # a rollback mechanism needs to be created here
+                        ###############################################
+                        flag = False
+                        break
+
+                #############################################
+                # requesting writing lock for the whole website
+                #############################################
+                # update log of accessing to productordered
+                # update procutsordered of user with(our_tID,pID,ourAmount)
+                # update log of down inventory
+                # update productsinventory with inventory = invenotry - ourAmount
+
+
+        #######################################################
+        # checking if all sites were successfully if not rollback
+        #######################################################
